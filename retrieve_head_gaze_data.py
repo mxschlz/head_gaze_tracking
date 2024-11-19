9e09f530dd03c81268123b2cafc4c58827ead213
max <schulz.max5@gmail.com>
2024-11-19 16:11:48 +0100

    Update

--- retrieve_head_gaze_data.py
+++ retrieve_head_gaze_data.py
@@ -2,16 +2,16 @@ from HeadGazeTracker.HeadGazeTracker import HeadGazeTracker
 
 
 subject = "sub-99"  # subject id
-video_input = f"/home/max/data/eeg/raw/{subject}_block_1.asf"  # if webcam==None, use this variable as video input
+video_input = f"/home/max/Downloads/max_hgt_output.avi"  # if webcam==None, use this variable as video input
 webcam = None # can be 0 or None
-video_output = f"/home/max/data/behavior/SPACEPRIME/tracking_log_data/{subject}_hgt_output.avi"
-tracking_data_log_folder = "/home/max/data/behavior/SPACEPRIME/tracking_log_data/"
+video_output = "output.avi"
+tracking_data_log_folder = "/home/max/PycharmProjects/head_gaze_tracking/test/"
 tracker = HeadGazeTracker(subject_id=subject, config_file_path="config.yml", WEBCAM=webcam,
                           VIDEO_INPUT=video_input,
                           VIDEO_OUTPUT=video_output,
                           TRACKING_DATA_LOG_FOLDER=tracking_data_log_folder,
-                          starting_timestamp=20240926175836189030,
-                          total_frames=3727)
+                          starting_timestamp=None,
+                          total_frames=None)
 
 # run
 tracker.run()
