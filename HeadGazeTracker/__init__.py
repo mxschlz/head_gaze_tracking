b3fc0a3298988898b75c9750661e34593e2501e1
Max <schulz.max5@gmail.com>
2025-11-07 12:53:23 +0100

    Updates data path for the BABYGAZE dataset.

--- HeadGazeTracker/__init__.py
+++ HeadGazeTracker/__init__.py
@@ -3,9 +3,9 @@ import os
 def get_data_path():
   """Returns the correct file path to the EEG data based on the OS."""
   if os.name == 'nt':  # Windows
-    data_path = 'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\'
+    data_path = 'G:\\Meine Ablage\\PhD\\data\\BABYGAZE\\'
   elif os.name == 'posix':  # Linux
-    data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/'
+    data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/BABYGAZE/'
   else:
     raise OSError("Unsupported operating system.")
   return data_path
